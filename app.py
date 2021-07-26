8fe6276b581f5da34090e6d844574d00ce6068bb
Ruby Hoseinzadeh <robina.sre78@gmail.com>
2021-07-26 13:59:14 -0700

    modified on reademe file

--- app.py
+++ app.py
@@ -26,8 +26,7 @@ db.create_all()
 
 BASE_URL = "https://api.adzuna.com/v1/api"
 BASE_PARAMS = "&results_per_page=20&content-type=application/json"
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
+
 APP_ID = os.getenv('APP_ID')
 APP_KEY = os.getenv('APP_KEY')
 
